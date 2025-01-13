0a638ffda5f7276f665ee0b191c96965981061e6
Spidey00007 <asamit1547k@gmail.com>
2025-01-13 15:16:37 +0530

    added clerk login authentication

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+function Dashboard() {
+  return <div>
+    Dashboard
+    <UserButton />
+  </div>;
+}
+
+export default Dashboard;
