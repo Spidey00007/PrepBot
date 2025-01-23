11f0526567b00e14bcb77ac29e92333b6c0af70c
Spidey00007 <asamit1547k@gmail.com>
2025-01-23 19:49:09 +0530

    Modified home page and header

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -3,28 +3,34 @@ import React from "react";
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 function Header() {
   const path = usePathname();
+  const router = useRouter();
 
   return (
-    <div className="flex p-4 items-center justify-between bg-secondary shadow-sm">
-      <Image src="/logo.svg" height={80} width={120} alt="logo" />
-      <ul className="hidden md:flex gap-6">
+    <div className="flex p-3 items-center bg-secondary justify-between shadow-sm">
+      <Image
+        className="cursor-pointer"
+        onClick={() => router.replace("/")}
+        src="/logo.svg"
+        height={80}
+        width={120}
+        alt="logo"
+      />
+      {/* <ul className="hidden md:flex gap-6">
         <li
+          onClick={() => router.replace("/dashboard")}
           className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${
             path === "/dashboard" && "text-primary font-bold"
           }`}
         >
           Dashboard
         </li>
-        <li
-          className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${
-            path === "/dashboard/help" && "text-primary font-bold"
-          }`}
-        >
-          Help
-        </li>
+        
         <li
           className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${
             path === "/dashboard/contact" && "text-primary font-bold"
@@ -32,8 +38,14 @@ function Header() {
         >
           Contact
         </li>
-      </ul>
-      <UserButton />
+      </ul> */}
+
+      <div className="flex p-4 items-center gap-12">
+        <Link href={"/dashboard"}>
+          <Button>Get Started</Button>
+        </Link>
+        <UserButton />
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,41 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import Header from "./dashboard/_components/Header";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div>
-      Hello Asamit!
-      <Button>Click me</Button>
+      <Header />
+
+      <div className="mx-auto max-w-screen-xl px-4 py-6 lg:flex lg:h-screen">
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-2xl text-gray-400 font-bold sm:text-5xl">
+            Introducing
+            <strong className="font-extrabold text-primary sm:block">
+              MockInterview AI
+            </strong>
+          </h1>
+
+          <p className="mt-4 text-gray-400 sm:text-xl">
+            Your go-to platform for practicing and improving your interview
+            skills with AI-generated interviews.
+          </p>
+
+          <img className="py-3" src="/InterviewHome.webp"></img>
+
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <Button
+              onClick={() => router.replace("/dashboard")}
+              className="items-center rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-purple-600 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+            >
+              Get Started
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
